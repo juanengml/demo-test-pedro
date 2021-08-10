91b3b71b15a465773597b5d41fb5d7a73dc6fa79
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-08-10 02:20:20 +0000

    Cores

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from console_logging.console import Console
+console = Console()
+
+console.log("APP iniciando...")
+
 total = totalmil = menor = produtos = 0
 barato = ""
 while True:
@@ -16,6 +21,8 @@ while True:
     if continuar == "N":
         print("{:-^40}".format("FIM DO PROGRAMA"))
         break
-print(f"O total gasto na compra foi de R${total:.2f}")
-print(f"O total de produtos acima de R$1000.00 é de: {totalmil}")
-print(f"O produto mais barato é o/a {barato} que custa R${menor:.2f}")
+console.info(f"O total gasto na compra foi de R${total:.2f}")
+console.info(f"O total de produtos acima de R$1000.00 é de: {totalmil}")
+console.info(f"O produto mais barato é o/a {barato} que custa R${menor:.2f}")
+
+console.success("DONE ! ")
