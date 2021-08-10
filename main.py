8ce3c6cdeb066410277d719e8be85bea662b7498
Pedro Bomfim <85983211+B0MFIM@users.noreply.github.com>
2021-08-10 02:13:16 +0000

    pequeno sisteminha de cadastro :p

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from requests import get 
-
-url = "https://random-data-api.com/api/restaurant/random_restaurant"
-data = {"size":2}
-
-r = get(url,data).json()
-
-for i in r:
-  print(i["id"])
-  for dia in i["hours"]:
-     if i["hours"][dia]["is_closed"] == True:
-         print(dia,"Aberto")
-     else:
-         print(dia, "fechado")
+pessoas = homens = mulheres = 0
+while True:
+    print("=" * 20)
+    print("CADASTRE UMA PESSOA")
+    print("=" * 20)
+    idade = int(input("Idade: "))
+    sexo = " "
+    while sexo not in "MF":
+        sexo = str(input("Sexo: [M/F] ")).strip().upper()[0]
+    print("=" * 20)
+    if idade >= 18:
+        pessoas += 1
+    if sexo == "M":
+        homens += 1
+    if sexo == "F" and idade < 20:
+        mulheres += 1
+    continuar = " "
+    while continuar not in "SN":
+        continuar = str(input("Quer continuar? [S/N] ")).strip().upper()[0]
+    if continuar == "N":
+        print("Finalizado!!")
+        break
+print(f"Temos um total de {pessoas} pessoa(s) maior(es) de 18 anos!!")
+print(f"Temos um total de {homens} Homens cadastrados no sistema!!")
+print(f"Temos um total de {mulheres} Mulheres menores de 20 anos!!")
